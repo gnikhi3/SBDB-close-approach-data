d630ae4b975d4aeeae1777d839d82810a965c790
gnikhi3 <96861565+gnikhi3@users.noreply.github.com>
2022-01-10 19:16:50 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,6 +5,7 @@ set -e
 python -m http.server 7000 &
 
 # create data.json file to get expected data
+echo 'Creating data.json, this might take couple of minutes please wait'
 python qa_utilities/get_test_data.py
 
 bash
